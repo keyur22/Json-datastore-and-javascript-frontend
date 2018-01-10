02bf50002ffa91002d69df3e6cc8c8edee67734e
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2018-01-10 11:43:59 +0530

    Add files via upload

---
+++ Products_Description.js
@@ -0,0 +1,259 @@
+var description =
+{
+     "summary" :
+     {
+          "id" : 125178,
+          "name" : "Redmi Note 4 (Black, 64 GB) (4 GB RAM)",
+          "image" : "https://rukminim1.flixcart.com/image/312/312/mobile/y/u/a/mi-redmi-note-4-na-original-imaeqdxgrdhxgkcx.jpeg?q=70",
+          "price" :10999,
+          "rating" : 4.4,
+          "ratingsNo" : 470852,
+          "reviewsNo" : 99284,
+          "flipkartAssured" : true,
+          "offers" : {
+                                  "emi" : "No Cost EMIs from ₹1,834/month.Other EMIs from ₹534/month",
+                                  "exchange" : "Get upto ₹10,000 off on exchange",
+                                  "specialPrice" : "Get extra ₹2000 off",
+                                  "phonePe" : "10% cashback on payments through PhonePe",
+                                  "jio" : "JIO Special Offer: Get 60GB Additional 4G Data"
+                         },
+          "warranty" : "1 Year for Mobile & 6 Months for Accessories",
+          "color" : [
+                              {
+                                   "name" : "Black",
+                                   "image" : "https://rukminim1.flixcart.com/image/832/832/mobile/f/d/5/mi-redmi-note-4-na-original-imaeqdxqcrfshtqu.jpeg?q=70"
+                              },
+                              {
+                                   "name" : "Dark Grey",
+                                   "image" : "https://rukminim1.flixcart.com/image/832/832/mobile/9/8/z/mi-redmi-note-4-na-original-imaeqdxjuadhagfc.jpeg?q=70"
+                              },
+                              {
+                                   "name" : "Gold",
+                                   "image" : "https://rukminim1.flixcart.com/image/832/832/mobile/y/u/a/mi-redmi-note-4-na-original-imaeqdxgrdhxgkcx.jpeg?q=70"
+                              }
+                         ],
+          "highlights" : {
+                                        "memory" : {
+                                                                    "ram" : "4 GB",
+                                                                    "rom" : "64 GB",
+                                                                    "expandableStorage" : "128 GB"
+                                                           },
+                                        "camera" : {
+                                                                     "rear" : "13 MP",
+                                                                     "front" : "5 MP"
+                                                         },
+                                        "display" : {
+                                                                     "size" : "5.5 inch",
+                                                                     "type" : "Full HD"
+                                                         },
+                                        "batteryCapacity" : "4100mAh",
+                                        "processor" : {
+                                                                     "type" : "Qualcomm Snapdragon 625 64-bit Octacore 2 GHz",
+                                                                     "core": "Octa Core",
+                                                                     "clockSpeed" : "2 GHz"
+                                                            }
+                                 },
+          "seller" : {
+                                  "name" : "Truenet Commerce",
+                                  "rating" : 4.5
+                         }
+     },
+
+     "specifications" :
+     {
+          "general":
+          {
+                   "inTheBox" : [   "Handset", "USB Cable", "Adapter", "SIM Tray Remover Pin", "User Guide", "Warranty Card" ],
+                   "modelNo" : [   "MZB5254IN", "MZB5545IN"      ],
+                   "modelName" : "Redmi Note 4",
+                   "color" : "Black",
+                   "simType" : "Dual Sim",
+                   "hybridSimSlot" : true,
+                   "otgCompatible" : true,
+                   "soundEnhancements" : [  "One Speaker (Bottom Opening)", "Active Noise Cancellation"    ]
+          },
+          "display" :
+          {
+                   "size" : "5.5 inch",
+                   "resolution" : "1920 x 1080 pixels",
+                   "resolutionType" : "Full HD",
+                   "gpu" : "Adreno 506"
+           },
+          "processor" :
+          {
+                   "type" : "Qualcomm Snapdragon 625 64-bit Octa Core 2GHz",
+                   "core" :  "Octacore",
+                   "speed" : "2 GHz"
+          },
+          "storage" :
+          {
+                   "internal" : "64 GB",
+                   "ram" : "4 GB",
+                   "expandable" : "128 GB"
+          },
+          "memoryCard" :
+          {
+                   "type" : "microSD",
+                   "slot" : "hybrid"
+          },
+          "camera" :
+          {
+                   "primary" : "13 MP",
+                   "primaryFeatures" : [     "f2.0 Aperture", "0.1s Ultra-fast PDAF", "CMOS Camera", "1.12 micrometre Larger Pixels",
+                                                    "720p at 120 fps Slow-motion Video Recording", "5p Lens Element"     ],
+                   "secondary" : "5 MP",
+                   "secondaryFeatures" : [   "2.0 Aperture", "Face Recognition", "1080p Full HD Video Recording", "Smart and Pro Beautify"      ]
+          },
+          "battery" : "4100mAh",
+          "dimensions" :
+          {
+                   "width" :  "76 mm",
+                   "height" : "151 mm",
+                   "depth" : "8.4 mm",
+                   "weight" : "165 g"
+          },
+          "warranty" : "1 Year for Mobile & 6 Months for Accessories"
+     },
+
+     "ratingsAndReviews" :
+     {
+          "ratings":
+          {
+               "rating" :
+               {
+                    "overall" : 4.4,
+                    "camera" : 3.3,
+                    "battery" : 4.4,
+                    "display" : 4,
+                    "valueForMoney" : 4.8
+               },
+               "ratingsCount" :
+               {
+                    "total" : 470852,
+                    "5Star" : 311074,
+                    "4Star" : 97850,
+                    "3Star" : 25497,
+                    "2Star" : 9563,
+                    "1Star" : 26868
+               },
+               "reviewsCount" : 99824
+          },
+          "reviews" :
+          [
+               {
+                    "rating" : 5,
+                    "review" : "Best in The Market",
+                    "description" : [
+                                                  "My Review in Abbreviation!", "R - Raging Speed" , "E - Excellent Performance" , "D - Dynamic Display" , "M - Metal Build",
+                                                  "I - Incredible Battery Life" , "N - Noteworthy", "O - Outstanding" , "T - Top notch specs" , "E - Economical",
+                                                  "4 - Fabulous, Fantastic, Furious and Futuristic!", "Worth every single penny!"
+                                           ],
+                    "postedBy" : {
+                                                  "firstName" : "Navin",
+                                                  "lastName" : "Kumar"
+                                           },
+                    "postedOn" : {
+                                                  "day" : 18,
+                                                  "month" : "March",
+                                                  "year" : 2017
+                                             },
+                    "likesCount" : 23001,
+                    "dislikesCount" : 3812
+               },
+               {
+                    "rating" : 5,
+                    "review" : "Awesome",
+                    "description" : [
+                                                  " Superb phone ..", "Built quality excellent..", "Jio runs superb ... very fast", "Small disappointment with back camera ..",
+                                                  "Front camera is ok..", "Sleek design....", "Out of 64gb 52 gb is available..", "Hats off for the fast delivery by flipkart..",
+                                                  "Overall satisfied with the phone..."
+                                           ],
+                    "postedBy" : {
+                                                  "firstName" : "Raja",
+                                                  "lastName" : "Sekar"
+                                           },
+                    "postedOn" : {
+                                                  "day" : 28,
+                                                  "month" : "January",
+                                                  "year" : 2017
+                                             },
+                    "likesCount" : 20167,
+                    "dislikesCount" : 3351
+               },
+               {
+                    "rating" : 5,
+                    "review" : "Terrific",
+                    "description" : [
+                                                  "Excellent Phone in this Price.", "1. Battery Life Excellent", "2. Camera Quality is good", "3. Processor Lightning Fast",
+                                                  "4. Screen Quality is Excellent", "5. 64 GB and 4 GB Combination is perfect", "Just Go for it"
+                                           ],
+                    "postedBy" : {
+                                                  "firstName" : "Nitin",
+                                                  "lastName" : "Chanda"
+                                           },
+                    "postedOn" : {
+                                                  "day" : 6,
+                                                  "month" : "February",
+                                                  "year" : 2017
+                                             },
+                    "likesCount" : 21851,
+                    "dislikesCount" : 4040
+               }
+          ]
+     },
+
+     "questionsAndAnswers" :
+     [
+          {
+               "question" : "Does it support VoLTE?",
+               "answer" : "Yes, it does. It works with Jio SIM as well.",
+               "likesCount" : 18083,
+               "dislikesCount" : 2884,
+               "postedBy" : {
+                                        "userType" : "Flipkart Seller",
+                                        "firstName" : "W S Retail"
+                                   }
+          },
+          {
+               "question" : "Does it have second space feature?",
+               "answer" : "Yes its have second space feature...",
+               "likesCount" : 31,
+               "dislikesCount" : 4,
+               "postedBy" : {
+                                        "userType" : "Certified Buyer",
+                                        "firstName" : "Piyush",
+                                        "lastName" : "Thakkar"
+                                   }
+          }
+     ]
+}
+
+var display = function (description){
+     console.log(description);
+}
+
+var searchSpecs = function (search){
+     if ( description.specifications[search] == undefined ){
+          console.log("invalid specifications entered");
+     }
+     else{
+          console.log(description.specifications[search]);
+     }
+}
+
+var searchQuestions = function (index){
+     if ( description.questionsAndAnswers[index] == undefined ){
+          console.log("Invalid Index!!");
+     }
+     else{
+          console.log(description.questionsAndAnswers[index]);
+     }
+}
+
+display(description);
+
+var search = "display";
+searchSpecs(search);
+
+var index = 1;
+searchQuestions(index);
---
+++ Products_List.js
@@ -0,0 +1,135 @@
+var list =
+[
+     {
+          "id" : 89723,
+          "name" : "Lenovo K8 Plus (Venom Black, 32 GB)",
+          "price" : 8999,
+          "imageUrl" : "https://rukminim1.flixcart.com/image/312/312/j7gi6q80/mobile/p/t/d/lenovo-k8-plus-pa8c0003in-original-imaexp2rupxj9w5f.jpeg?q=70",
+          "ratingsCount" : 5857,
+          "reviewsCount" : 1729,
+          "rating" : 4.1,
+          "specs" : {
+                                   "memory" : {
+                                                                "ram" : "3 GB",
+                                                                "rom" : "32 GB",
+                                                                "expandableStorage" : "128 GB"
+                                                      },
+                                   "camera" : {
+                                                                 "rear" : "13 MP + 5 MP",
+                                                                 "front" : "8 MP"
+                                                    },
+                                   "display" : {
+                                                                "size" : "5.2 inch",
+                                                                "type" : "Full HD"
+                                                    },
+                                   "batteryCapacity" : "4000mAh",
+                                   "processor" : {
+                                                                "type" : "MediaTek MTK Helio P25 Octa Core 2.5 GHz",
+                                                                "core": "Octa Core",
+                                                                "clockSpeed" : "2.5 GHz"
+                                                         },
+                                   "warranty" : "1 Year on Handset, 6 Months on Accessories",
+                                   "operatingSystem" : "Android Nougat 7.1.1"
+                         }
+     },
+     {
+          "id" : 125178,
+          "name" : "Redmi Note 4 (Gold, 64 GB)",
+          "price" : 10999,
+          "imageUrl" : "https://rukminim1.flixcart.com/image/312/312/mobile/y/u/a/mi-redmi-note-4-na-original-imaeqdxgrdhxgkcx.jpeg?q=70",
+          "ratingsCount" : 470852,
+          "reviewsCount" : 99284,
+          "rating" : 4.4,
+          "specs" : {
+                                   "memory" : {
+                                                               "ram" : "4 GB",
+                                                               "rom" : "64 GB",
+                                                               "expandableStorage" : "128 GB"
+                                                      },
+                                   "camera" : {
+                                                                "rear" : "13 MP",
+                                                                "front" : "5 MP"
+                                                    },
+                                   "display" : {
+                                                                "size" : "5.5 inch",
+                                                                "type" : "Full HD"
+                                                    },
+                                   "batteryCapacity" : "4100mAh",
+                                   "processor" : {
+                                                                "type" : "Qualcomm Snapdragon 625 64-bit Octacore 2 GHz",
+                                                                "core": "Octa Core",
+                                                                "clockSpeed" : "2 GHz"
+                                                       },
+                                   "warranty" : "1 Year on Handset, 6 Months on Accessories",
+                                   "operatingSystem" : "Android Marshmallow 6.0.1"
+                         }
+     },
+     {
+          "id" : 8236,
+          "name" : "Moto G5 Plus (Lunar Gary, 32 GB)",
+          "price" : 13999,
+          "imageUrl" : "https://rukminim1.flixcart.com/image/312/312/mobile/5/b/f/motorola-moto-g5-plus-xt-1686-original-imaes3bzhs4thksp.jpeg?q=70",
+          "ratingsCount" : 105107,
+          "reviewsCount" : 26241,
+          "rating" : 4.3,
+          "specs" : {
+
+                                   "memory" : {
+                                                                "ram" : "4 GB",
+                                                                "rom" : "32 GB",
+                                                                "expandableStorage" : "128 GB"
+                                                       },
+                                   "camera" : {
+                                                                "rear" : "12 MP",
+                                                                "front" : "5 MP"
+                                                    },
+                                   "display" : {
+                                                                "size" : "5.2 inch",
+                                                                "type" : "Full HD"
+                                                    },
+                                   "batteryCapacity" : "3000mAh",
+                                   "processor" : {
+                                                                "type" : "Qualcomm Snapdragon 625 Octacore 2 GHz",
+                                                                "core": "Octa Core",
+                                                                "clockSpeed" : "2 GHz"
+                                                          },
+                                   "warranty" : "1 Year on Handset, 6 Months on Accessories",
+                                   "operatingSystem" : "Android Nougat 7"
+                         }
+     }
+]
+
+
+var display = function (list){
+     console.log(list);
+}
+
+var searchById = function (list, id){
+     found = 0;
+     list.map(function (x){
+          if ( x.id == id ){
+               console.log(x.name);
+               found = 1;
+          }
+     })
+     if ( found == 0 ){
+          console.log("Item Not Found");
+     }
+}
+
+var searchByIndex = function (list, index){
+     if ( list[index] == undefined ){
+          console.log("invalid Index!!!");
+     }
+     else {
+          console.log(list[index].name);
+     }
+}
+
+display(list);
+
+var id = 8236;
+searchById(list, id);
+
+var index = 1;
+searchByIndex(list, index);
